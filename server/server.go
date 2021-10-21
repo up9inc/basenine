f332d648103b24eecb09f47dfe9f36b103edf234
M. Mert Yildiran <mehmet@up9.com>
2021-10-21 20:40:41 +0300

    Document `server/server.go` with comments

--- server/server.go
+++ server/server.go
@@ -1,3 +1,17 @@
+// Copyright 2021 UP9. All rights reserved.
+// Use of this source code is governed by Apache License 2.0
+// license that can be found in the LICENSE file.
+//
+// Package main implements a schema-free, streaming database that
+// also defines a TCP-based protocol. Please refer to the client libraries
+// for communicating with the server.
+//
+// The server can be with a command like below:
+//
+//   basenine -addr -addr 127.0.0.1 -port 8000
+//
+// which sets the host address and TCP port.
+//
 package main
 
 import (
@@ -23,6 +37,22 @@ var port = flag.Int("port", 8000, "The port to listen on; default is 8000.")
 
 type ConnectionMode int
 
+// The modes of TCP connections that the clients can use.
+//
+// INSERT is a long lasting TCP connection mode for inserting data into database.
+//
+// QUERY is a long lasting TCP connection mode for retrieving data from the database
+// based on a given filtering query.
+//
+// SINGLE is a short lasting TCP connection mode for fetching a single record from the database.
+//
+// VALIDATE is a short lasting TCP connection mode for validating a query against syntax errors.
+//
+// MACRO is short lasting TCP connection mode for setting a macro that will be expanded
+// later on for each individual query.
+//
+// LIMIT is short lasting TCP connection mode for setting the maximum database size
+// to limit the disk usage.
 const (
 	NONE ConnectionMode = iota
 	INSERT
@@ -35,6 +65,7 @@ const (
 
 type Commands int
 
+// Commands refers to TCP connection modes.
 const (
 	CMD_INSERT   string = "/insert"
 	CMD_QUERY    string = "/query"
@@ -44,15 +75,32 @@ const (
 	CMD_LIMIT    string = "/limit"
 )
 
+// Constants defines the database filename's prefix and file extension.
 const DB_FILE string = "data"
 const DB_FILE_EXT string = "bin"
 
+// Initial value of database size limit. 0 means unlimited size.
 var dbSizeLimit int64 = 0
 
+// Slice that stores the TCP connections
 var connections []net.Conn
 
 var cs ConcurrentSlice
 
+// ConcurrentSlice is a mutually excluded struct that contains a list of fields that
+// needs to be safely accessed data across multiple goroutines.
+//
+// lastOffset contains the offset of the latest inserted record into the database.
+//
+// partitionRefs is a slice that contains the corresponding partition references of offsets.
+//
+// offsets is a slice that contains the offsets of each individual records inserted into the database.
+//
+// partitions is a slice of file descriptors that refers to the database partitions.
+//
+// partitionIndex is the current index of currently being inserted database partition. Which
+// is a reference to partitions slice's index and it's often times equal to len(partitions).
+// Initial value of partitionIndex should be -1. -1 means there are no partitions yet.
 type ConcurrentSlice struct {
 	sync.RWMutex
 	lastOffset     int64
@@ -62,24 +110,33 @@ type ConcurrentSlice struct {
 	partitionIndex int64
 }
 
+// Main method that the program enters into
 func main() {
+	// Parse the command-line arguments.
 	flag.Parse()
 
 	fmt.Println("Starting server...")
+
+	// Clean up the database files.
 	removeDatabaseFiles()
+
+	// Initialize the ConcurrentSlice.
 	cs = ConcurrentSlice{
 		partitionIndex: -1,
 	}
 
+	// Start listenning to given address and port.
 	src := *addr + ":" + strconv.Itoa(*port)
 	listener, _ := net.Listen("tcp", src)
 	fmt.Printf("Listening on %s.\n", src)
 
 	defer listener.Close()
 
+	// Make a channel to gracefully close the TCP connections.
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
+	// Handle the channel.
 	go func() {
 		<-c
 		quitConnections()
@@ -87,19 +144,23 @@ func main() {
 		os.Exit(1)
 	}()
 
+	// Start accepting TCP connections.
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Printf("Some connection error: %s\n", err)
 		}
 
+		// Handle the TCP connection.
 		go handleConnection(c, conn)
 	}
 }
 
+// newParitition crates a new database paritition. The filename format is data_000000000.bin
+// Such that the filename increments according to the partition index.
 func newPartition() *os.File {
 	cs.Lock()
-	cs.partitionIndex += 1
+	cs.partitionIndex++
 	f, err := os.OpenFile(fmt.Sprintf("%s_%09d.%s", DB_FILE, cs.partitionIndex, DB_FILE_EXT), os.O_CREATE|os.O_WRONLY, 0644)
 	check(err)
 	cs.partitions = append(cs.partitions, f)
@@ -108,6 +169,7 @@ func newPartition() *os.File {
 	return f
 }
 
+// removeDatabaseFiles cleans up all of the database files.
 func removeDatabaseFiles() {
 	files, err := filepath.Glob("./data_*.bin")
 	check(err)
@@ -117,6 +179,9 @@ func removeDatabaseFiles() {
 	}
 }
 
+// periodicPartitioner is a Goroutine that handles database parititioning according
+// to the database size limit that's set by /limit command.
+// Triggered every second.
 func periodicPartitioner(f *os.File) {
 	for {
 		time.Sleep(1 * time.Second)
@@ -127,9 +192,14 @@ func periodicPartitioner(f *os.File) {
 		check(err)
 		currentSize := info.Size()
 		if currentSize > dbSizeLimit {
+			// If we exceeded the half of the database size limit, create a new partition
 			f = newPartition()
+
+			// Safely access the partitions slice and partitionIndex
 			cs.Lock()
 			if cs.partitionIndex > 1 {
+				// There can be only two living partition any given time.
+				// We've created the third partition, so discard the first one.
 				discarded := cs.partitions[cs.partitionIndex-2]
 				discarded.Close()
 				os.Remove(discarded.Name())
@@ -140,6 +210,9 @@ func periodicPartitioner(f *os.File) {
 	}
 }
 
+// periodicFileSyncer is a Goroutine that handles
+// commiting the current contents of the file to stable storage
+// by calling f.Sync() every 10 milliseconds.
 func periodicFileSyncer(f *os.File) {
 	for {
 		time.Sleep(10 * time.Millisecond)
@@ -150,27 +223,37 @@ func periodicFileSyncer(f *os.File) {
 	}
 }
 
+// handleConnection handles a TCP connection
 func handleConnection(c chan os.Signal, conn net.Conn) {
+	// Append connection into a global slice
 	connections = append(connections, conn)
+
+	// Log the connection
 	remoteAddr := conn.RemoteAddr().String()
 	fmt.Println("Client connected from " + remoteAddr)
 
+	// Create a scanner
 	scanner := bufio.NewScanner(conn)
 	buf := make([]byte, 0, 64*1024)
+
+	// Prevent buffer overflows
 	scanner.Buffer(buf, 209715200)
 
+	// Set connection mode to NONE
 	var mode ConnectionMode = NONE
 	var f *os.File
 
 	defer f.Close()
 
 	for {
+		// Safely access the current partition index and get the current partition
 		cs.RLock()
 		if cs.partitionIndex > -1 {
 			f = cs.partitions[cs.partitionIndex]
 		}
 		cs.RUnlock()
 
+		// Scan the input
 		ok := scanner.Scan()
 
 		if !ok {
@@ -179,8 +262,10 @@ func handleConnection(c chan os.Signal, conn net.Conn) {
 			break
 		}
 
+		// Handle the message
 		_mode, data := handleMessage(scanner.Text(), conn)
 
+		// Set the connection mode
 		switch mode {
 		case NONE:
 			mode = _mode
@@ -204,19 +289,25 @@ func handleConnection(c chan os.Signal, conn net.Conn) {
 		}
 	}
 
+	// Log the disconnect
 	fmt.Println("Client at " + remoteAddr + " disconnected.")
 
+	// In case of an INSERT mode client is disconnected, clean up the database files.
 	if mode == INSERT {
 		removeDatabaseFiles()
 	}
 }
 
+// quitConnections quits all of the active TCP connections. It's only called
+// in case of an interruption.
 func quitConnections() {
 	for _, conn := range connections {
 		conn.Write([]byte("%quit%\n"))
 	}
 }
 
+// handleMessage handles given message string of a TCP connection and returns a
+// ConnectionMode to set the mode of the that TCP connection.
 func handleMessage(message string, conn net.Conn) (mode ConnectionMode, data []byte) {
 	fmt.Println("> " + message)
 
@@ -251,12 +342,19 @@ func handleMessage(message string, conn net.Conn) (mode ConnectionMode, data []b
 	return
 }
 
+// check throws a panic if the given error is not nil.
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+// insertData inserts a record into database.
+// It unmarshals the given bytes into a map[string]interface{}
+// Then inserts a key named "id" to that map. Which indicates the
+// index of that record.
+// Then marshals that map back and safely writes the bytes into
+// the current database partitition.
 func insertData(f *os.File, data []byte) {
 	var d map[string]interface{}
 	if err := json.Unmarshal(data, &d); err != nil {
@@ -264,23 +362,37 @@ func insertData(f *os.File, data []byte) {
 	}
 
 	var lastOffset int64
+	// Safely access the last offset
 	cs.RLock()
 	l := len(cs.offsets)
 	lastOffset = cs.lastOffset
 	cs.RUnlock()
+
+	// TODO: Replace this with a substructure that serves as a metadata field.
+	// Set "id" field to the index of the record.
 	d["id"] = l
+
+	// Marshal it back.
 	data, _ = json.Marshal(d)
 
+	// Calculate the length of bytes.
 	var length int64 = int64(len(data))
-
 	b := make([]byte, 8)
 	binary.LittleEndian.PutUint64(b, uint64(length))
 
+	// Prepend the length into the data.
 	data = append(b, data...)
+
+	// Write the record into database immediately after the last record.
+	// The offset is tracked by lastOffset which is cs.lastOffset
+	// WriteAt() is important here! Write() races.
 	n, err := f.WriteAt(data, lastOffset)
 	check(err)
+
+	// Log the amount of bytes that are written into the database.
 	fmt.Printf("wrote %d bytes\n", n)
 
+	// Safely update the offsets and paritition references.
 	cs.Lock()
 	cs.offsets = append(cs.offsets, lastOffset)
 	cs.partitionRefs = append(cs.partitionRefs, cs.partitionIndex)
@@ -288,6 +400,8 @@ func insertData(f *os.File, data []byte) {
 	cs.Unlock()
 }
 
+// readRecord reads the record from the database paritition provided by argument f
+// and the reads the record by seeking to the offset provided by seek argument.
 func readRecord(f *os.File, seek int64) (b []byte, n int64, err error) {
 	n = seek
 	l := make([]byte, 8)
@@ -310,51 +424,83 @@ func readRecord(f *os.File, seek int64) (b []byte, n int64, err error) {
 	return
 }
 
+// streamRecords is an infinite loop that only called in case of QUERY TCP connection mode.
+// It expands marcros, parses the given query, does compile-time evaluations with Precompute() call
+// and filters out the records according to query.
+// It starts from the very beginning of the first living database partition.
+// Means that either the current partition or the partition before that.
 func streamRecords(conn net.Conn, data []byte) (err error) {
 	query := string(data)
+
+	// Expand all macros in the query, if there are any.
 	query, err = expandMacros(query)
 	check(err)
+
+	// Parse the query.
 	expr, err := Parse(query)
 	check(err)
+
+	// Do compile-time evaluations.
 	err = Precompute(expr)
 	check(err)
 
+	// n is the last offset we were reading
 	var n int64 = 0
-	var dbStartIndex int64 = -1
+	// -1 means that the paritition start index is not decided yet.
+	var partitionStartIndex int64 = -1
 
 	for {
 		time.Sleep(10 * time.Millisecond)
 
+		// Safely access the current partition index
 		cs.Lock()
 		partitionIndex := cs.partitionIndex
 		cs.Unlock()
 
+		// Paritition index -1 means there no partitions started yet.
+		// Means that newPartition() is not called yet.
 		if partitionIndex == -1 {
 			continue
 		}
 
-		if dbStartIndex > partitionIndex {
-			dbStartIndex = partitionIndex
+		// This condition is only true if the previous partition EOL-ed
+		// and the loop tries to continue with the next partition
+		// but there are no new partitions yet.
+		if partitionStartIndex > partitionIndex {
+			partitionStartIndex = partitionIndex
 		} else {
+			// When the condition becomes false, it means that we switched
+			// to the new partitition so we start to read from the
+			// very first offset, which is 0.
 			n = 0
 		}
 
-		if dbStartIndex == -1 {
-			dbStartIndex = partitionIndex
+		// If the partitionStartIndex is still -1, means that we're still
+		// in the very first iteration
+		if partitionStartIndex == -1 {
+			// Set partitionStartIndex to latest partition
+			partitionStartIndex = partitionIndex
+			// but if there is a partition before the latest one
+			// set it to the previous partition.
 			if partitionIndex > 1 {
-				dbStartIndex -= 1
+				partitionStartIndex--
 			}
 		}
 
+		// Safely access and open the partition decided by partitionStartIndex
 		cs.Lock()
-		f, err := os.Open(cs.partitions[dbStartIndex].Name())
+		f, err := os.Open(cs.partitions[partitionStartIndex].Name())
 		cs.Unlock()
 
+		// If the file cannot be opened, try again. The file should be there.
 		if err != nil {
 			continue
 		}
+
+		// Seek into the last offset that's tracked by n
 		f.Seek(n, 0)
 
+		// Read the records until seeing EOF
 		for {
 			var b []byte
 			b, n, err = readRecord(f, n)
@@ -362,42 +508,64 @@ func streamRecords(conn net.Conn, data []byte) (err error) {
 				break
 			}
 
+			// Evaluate the current record agains the given query.
 			truth, err := Eval(expr, string(b))
 			check(err)
 
+			// Write the record into TCP connection if it passes the query.
 			if truth {
 				conn.Write([]byte(fmt.Sprintf("%s\n", b)))
 			}
 		}
 
+		// Close the current partition.
 		f.Close()
-		dbStartIndex++
+
+		// Increment the partition index tracked by the this loop.
+		partitionStartIndex++
 	}
 }
 
+// retrieveSingle fetches a single record from the database.
 func retrieveSingle(conn net.Conn, data []byte) (err error) {
-	index, _ := strconv.Atoi(string(data))
+	// Convert index value provided as string to integer
+	index, err := strconv.Atoi(string(data))
+	if err != nil {
+		conn.Write([]byte(fmt.Sprintf("Error: While converting the index to integer: %s\n", err.Error())))
+		return
+	}
 
+	// Safely access the length of offsets slice.
 	cs.RLock()
 	l := len(cs.offsets)
 	cs.RUnlock()
 
+	// Check if the index is in the offsets slice.
 	if index >= l {
 		conn.Write([]byte(fmt.Sprintf("Index out of range: %d\n", index)))
 		return
 	}
 
+	// Safely acces the offsets and partition references
 	var f *os.File
 	cs.RLock()
 	n := cs.offsets[index]
 	i := cs.partitionRefs[index]
 	f, err = os.Open(cs.partitions[i].Name())
 	cs.RUnlock()
+
+	// Record can only be removed if the partition of the record
+	// that it belongs to is removed. Therefore a file open error
+	// means the record is removed.
+	// It can only occur if the dbSizeLimit is set to a value
+	// other than 0
 	if err != nil {
 		conn.Write([]byte(fmt.Sprintf("Record does not exist!\n")))
 		return
 	}
 
+	// If we got to this point then it means the record is there
+	// Read it using its offset (which is n) and return it.
 	f.Seek(n, 0)
 	var b []byte
 	b, n, err = readRecord(f, n)
@@ -405,6 +573,8 @@ func retrieveSingle(conn net.Conn, data []byte) (err error) {
 	return
 }
 
+// validateQuery tries to parse the given query and checks if there are
+// any syntax errors or not.
 func validateQuery(conn net.Conn, data []byte) {
 	query := string(data)
 	_, err := Parse(query)
@@ -416,6 +586,7 @@ func validateQuery(conn net.Conn, data []byte) {
 	}
 }
 
+// applyMacro defines a macro that will be expanded for each individual query.
 func applyMacro(conn net.Conn, data []byte) {
 	str := string(data)
 
@@ -433,11 +604,13 @@ func applyMacro(conn net.Conn, data []byte) {
 	conn.Write([]byte(fmt.Sprintf("OK\n")))
 }
 
+// setLimit sets a limit for the maximum database size.
 func setLimit(conn net.Conn, data []byte) {
 	value, err := strconv.Atoi(string(data))
 
 	if err != nil {
 		conn.Write([]byte(fmt.Sprintf("Error: While converting the limit to integer: %s\n", err.Error())))
+		return
 	}
 
 	dbSizeLimit = int64(value) / 2
