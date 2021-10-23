90a7eb80e9febd6e6314d0c0b85af387021ae159
M. Mert Yildiran <mehmet@up9.com>
2021-10-24 00:38:06 +0300

    Document the client library for Go with comments

--- client/go/client.go
+++ client/go/client.go
@@ -1,3 +1,9 @@
+// Copyright 2021 UP9. All rights reserved.
+// Use of this source code is governed by Apache License 2.0
+// license that can be found in the LICENSE file.
+//
+// This is the client library for Basenine databse server.
+//
 package main
 
 import (
@@ -11,10 +17,13 @@ import (
 	"time"
 )
 
+// Connection is the struct that holds the TCP connection reference.
 type Connection struct {
 	net.Conn
 }
 
+// NewConnection establishes a new connection with the server in the address host:port.
+// Returns a Connection reference and an error.
 func NewConnection(host string, port string) (connection *Connection, err error) {
 	dest := host + ":" + port
 	var conn net.Conn
@@ -23,19 +32,25 @@ func NewConnection(host string, port string) (connection *Connection, err error)
 	return
 }
 
+// Send sends given []byte to the server which the connection is established to.
 func (c *Connection) Send(data []byte) {
 	c.SetWriteDeadline(time.Now().Add(1 * time.Second))
 	c.Write(data)
 }
 
+// SendText is the wrapper around Send method that allows user to send text directly.
 func (c *Connection) SendText(text string) {
 	c.Send([]byte(fmt.Sprintf("%s\n", text)))
 }
 
+// InsertMode turns the connection's mode into INSERT mode
 func (c *Connection) InsertMode() {
 	c.SendText("/insert")
 }
 
+// Query is the method that user should use to stream the records from the database.
+// It takes the filtering language (query) as the first parameter and
+// a []byte channel which the records will be streamed into.
 func (c *Connection) Query(query string, data chan []byte) {
 	var wg sync.WaitGroup
 	go readConnection(&wg, c, data)
@@ -45,6 +60,8 @@ func (c *Connection) Query(query string, data chan []byte) {
 	c.SendText(fmt.Sprintf("%s", query))
 }
 
+// Single returns a single record from the database server specified by the host:port pair
+// and by given ID.
 func Single(host string, port string, id int) (data []byte, err error) {
 	var c *Connection
 	c, err = NewConnection(host, port)
@@ -65,6 +82,8 @@ func Single(host string, port string, id int) (data []byte, err error) {
 	return
 }
 
+// Validate validates the given query against syntax errors by passing the query
+// to the database server at host:port
 func Validate(host string, port string, query string) (err error) {
 	var c *Connection
 	c, err = NewConnection(host, port)
@@ -89,6 +108,8 @@ func Validate(host string, port string, query string) (err error) {
 	return
 }
 
+// Macro defines a macro that expands into expanded in the database server
+// at host:port. Same macro can be overwritten by a second Macro call.
 func Macro(host string, port string, macro string, expanded string) (err error) {
 	var c *Connection
 	c, err = NewConnection(host, port)
@@ -113,6 +134,9 @@ func Macro(host string, port string, macro string, expanded string) (err error) 
 	return
 }
 
+// Limit sets a hard-limit for the database size in bytes like 1000000 (1MB)
+// Such that actual disk usage ranges between half of the limit (soft-limit) and
+// the limit's itself (hard-limit).
 func Limit(host string, port string, limit int64) (err error) {
 	var c *Connection
 	c, err = NewConnection(host, port)
@@ -137,6 +161,8 @@ func Limit(host string, port string, limit int64) (err error) {
 	return
 }
 
+// readConnection is a Goroutine that recieves messages from the TCP connection
+// and send them to a []byte channel provided by the data parameter.
 func readConnection(wg *sync.WaitGroup, c *Connection, data chan []byte) {
 	defer wg.Done()
 	for {
@@ -164,6 +190,9 @@ func readConnection(wg *sync.WaitGroup, c *Connection, data chan []byte) {
 	}
 }
 
+// handleCommands is used by readConnection to make the server's orders
+// in the client to take effect. Such that the server can hang up
+// the connection.
 func handleCommands(bytes []byte) bool {
 	r, err := regexp.Compile("^%.*%$")
 	text := string(bytes)
