04e4a3b8bd161f45a5e085d2aa56d300303ebba9
M. Mert Yildiran <mehmet@up9.com>
2021-10-25 02:51:27 +0300

    Revert "Bring back `fsnotify`"
    This reverts commit 574e3eae6355340ee867bbadff2602ce02ae5021.

--- server/server.go
+++ server/server.go
@@ -31,8 +31,6 @@ import (
 	"sync"
 	"syscall"
 	"time"
-
-	"github.com/fsnotify/fsnotify"
 )
 
 var addr = flag.String("addr", "", "The address to listen to; default is \"\" (all interfaces).")
@@ -118,9 +116,6 @@ type ConcurrentSlice struct {
 	partitionIndex int64
 }
 
-// Global file watcher
-var watcher *fsnotify.Watcher
-
 func init() {
 	// Clean up the database files.
 	removeDatabaseFiles()
@@ -133,11 +128,6 @@ func init() {
 	// Trigger partitioning check for every second.
 	ticker := time.NewTicker(1 * time.Second)
 	go periodicPartitioner(ticker)
-
-	// Initiate the global watcher
-	var err error
-	watcher, err = fsnotify.NewWatcher()
-	check(err)
 }
 
 func main() {
@@ -167,7 +157,6 @@ func main() {
 	go func() {
 		<-c
 		quitConnections()
-		watcher.Close()
 		removeDatabaseFiles()
 		os.Exit(1)
 	}()
@@ -194,10 +183,6 @@ func newPartition() *os.File {
 	cs.partitions = append(cs.partitions, f)
 	cs.lastOffset = 0
 	cs.Unlock()
-
-	err = watcher.Add(f.Name())
-	check(err)
-
 	return f
 }
 
@@ -245,8 +230,6 @@ func periodicPartitioner(ticker *time.Ticker) {
 				// We've created the third partition, so discard the first one.
 				discarded := cs.partitions[cs.partitionIndex-2]
 				discarded.Close()
-				err = watcher.Remove(discarded.Name())
-				check(err)
 				os.Remove(discarded.Name())
 				cs.partitions[cs.partitionIndex-2] = nil
 			}
@@ -321,6 +304,11 @@ func handleConnection(conn net.Conn) {
 
 	// Log the disconnect
 	log.Println("Client at " + remoteAddr + " disconnected.")
+
+	// In case of an INSERT mode client is disconnected, clean up the database files.
+	if mode == INSERT {
+		removeDatabaseFiles()
+	}
 }
 
 // quitConnections quits all of the active TCP connections. It's only called
@@ -514,21 +502,7 @@ func streamRecords(conn net.Conn, data []byte) (err error) {
 	var leftOff int64 = 0
 
 	for {
-		// Block until a partition is modified
-		select {
-		case event, ok := <-watcher.Events:
-			if !ok {
-				return
-			}
-			if event.Op&fsnotify.Write != fsnotify.Write {
-				continue
-			}
-		case errW, ok := <-watcher.Errors:
-			if !ok {
-				return
-			}
-			check(errW)
-		}
+		time.Sleep(10 * time.Millisecond)
 
 		// f is the current partition we're reading the data from.
 		var f *os.File
