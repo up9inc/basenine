a0d1066a629bf53925a820bf2e6d81751019086b
M. Mert Yildiran <mehmet@up9.com>
2021-10-25 02:55:11 +0300

    Remove `fsnotify` related code from the tests

--- server/server_test.go
+++ server/server_test.go
@@ -39,8 +39,6 @@ func TestServerNewPartition(t *testing.T) {
 	assert.NotNil(t, f)
 	assert.FileExists(t, fmt.Sprintf("%s_%09d.%s", DB_FILE, cs.partitionIndex, DB_FILE_EXT))
 
-	err := watcher.Remove(f.Name())
-	assert.Nil(t, err)
 	removeDatabaseFiles()
 }
 
@@ -82,8 +80,6 @@ func TestServerInsertAndReadData(t *testing.T) {
 		rf.Close()
 	}
 
-	err := watcher.Remove(f.Name())
-	assert.Nil(t, err)
 	removeDatabaseFiles()
 }
 
@@ -129,8 +125,6 @@ func TestServerProtocolInsertMode(t *testing.T) {
 	assert.Greater(t, n, int64(0))
 	assert.Equal(t, expected, string(b))
 
-	err = watcher.Remove(rf.Name())
-	assert.Nil(t, err)
 	rf.Close()
 
 	client.Close()
@@ -198,8 +192,6 @@ func TestServerProtocolQueryMode(t *testing.T) {
 		client.Close()
 		server.Close()
 
-		err := watcher.Remove(f.Name())
-		assert.Nil(t, err)
 		removeDatabaseFiles()
 	}
 }
@@ -314,8 +306,6 @@ func TestServerProtocolValidateMode(t *testing.T) {
 			client.Close()
 			server.Close()
 
-			err := watcher.Remove(f.Name())
-			assert.Nil(t, err)
 			removeDatabaseFiles()
 		}
 	}
@@ -391,8 +381,6 @@ func TestServerProtocolMacroMode(t *testing.T) {
 		client.Close()
 		server.Close()
 
-		err := watcher.Remove(f.Name())
-		assert.Nil(t, err)
 		removeDatabaseFiles()
 	}
 }
@@ -447,15 +435,6 @@ func TestServerProtocolLimitMode(t *testing.T) {
 
 	client.Close()
 	server.Close()
-
-	cs.RLock()
-	f1 := cs.partitions[cs.partitionIndex]
-	f2 := cs.partitions[cs.partitionIndex-1]
-	err = watcher.Remove(f1.Name())
-	assert.Nil(t, err)
-	err = watcher.Remove(f2.Name())
-	assert.Nil(t, err)
-	cs.RUnlock()
 
 	removeDatabaseFiles()
 }
