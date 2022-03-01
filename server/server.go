d2111357b14e626acf0237ec857219bb37f6a16a
M. Mert Yildiran <mehmet@up9.com>
2022-03-01 16:59:11 +0300

    Bump the version to `0.5.0`

--- server/server.go
+++ server/server.go
@@ -47,7 +47,7 @@ var version = flag.Bool("version", false, "Print version and exit.")
 var persistent = flag.Bool("persistent", false, "Enable persistent mode. Dumps core on exit.")
 
 // Version of the software.
-const VERSION string = "0.4.17"
+const VERSION string = "0.5.0"
 
 type ConnectionMode int
 
