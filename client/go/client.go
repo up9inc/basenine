b9bcda60bef25a2e068bb2b1e26108bcb47c773f
M. Mert Yildiran <mehmet@up9.com>
2022-03-15 10:04:24 +0300

    Update copyright file header comments

--- client/go/client.go
+++ client/go/client.go
@@ -1,4 +1,4 @@
-// Copyright 2021 UP9. All rights reserved.
+// Copyright 2022 UP9. All rights reserved.
 // Use of this source code is governed by Apache License 2.0
 // license that can be found in the LICENSE file.
 //
